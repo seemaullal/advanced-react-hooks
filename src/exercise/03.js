995f3663f9e8e5dc02578a8398f1fe96afd36903
Seema Ullal <seema.ullal@gusto.com>
2020-10-09 22:24:42 -0700

    custom hook for useCount, context

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -5,18 +5,31 @@ import React from 'react';
 
 const CountContext = React.createContext();
 
+function useCount() {
+  const context = React.useContext(CountContext);
+  let count, setCount;
+  if (!context) {
+    throw new Error(
+      'useCount must be used inside of a component nested under a CountContext.Provider',
+    );
+  } else {
+    [count, setCount] = context;
+  }
+  return [count, setCount];
+}
+
 function CountProvider(props) {
   const [count, setCount] = React.useState(0);
   return <CountContext.Provider value={[count, setCount]} {...props} />;
 }
 
 function CountDisplay() {
-  const [count] = React.useContext(CountContext);
+  const [count] = useCount();
   return <div>{`The current count is ${count}`}</div>;
 }
 
 function Counter() {
-  const [, setCount] = React.useContext(CountContext);
+  const [, setCount] = useCount();
   const increment = () => setCount(c => c + 1);
   return <button onClick={increment}>Increment count</button>;
 }
